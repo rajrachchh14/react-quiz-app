d5c39353e4cb4858d0261321c7205bc5c1d11b87
rajrachchh14 <rajrachchh14@gmail.com>
2021-10-07 09:05:35 +0530

    1 file modified

--- src/SecondScreen.js
+++ src/SecondScreen.js
@@ -1,20 +1,19 @@
 import React from 'react';
 
 export default function SecondScreen() {
+
+
+  
   return (
     <>
       <div className="col-4 col-lg-4 col-md-4 ">
         <div className="fixed-right">
-          <div class="alert alert-light border border-primary">$ 100</div>
-          <div class="alert alert-light border border-primary">$ 200</div>
-          <div class="alert alert-light border border-primary">$ 300</div>
-          <div class="alert alert-light border border-primary">$ 400</div>
-          <div class="alert alert-light border border-primary">$ 500</div>
-          <div class="alert alert-light border border-primary">$ 600</div>
-          <div class="alert alert-light border border-primary">$ 700</div>
-          <div class="alert alert-light border border-primary">$ 800</div>
-          <div class="alert alert-light border border-primary">$ 900</div>
-          <div class="alert alert-light border border-primary">$ 1000</div>
+          <div
+            class="alert alert-light border border-primary"
+            style={{ padding: '5px' }}
+          >
+            1. $ 100
+          </div>
         </div>
       </div>
     </>
